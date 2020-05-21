a8268c6b3df04a98b770f225398fc57beebdc9cd
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-05-21 10:05:52 +0100

    Registered apps.

--- src/fabio-milheiro-root-config.js
+++ src/fabio-milheiro-root-config.js
@@ -4,7 +4,13 @@ import * as isActive from "./activity-functions";
 registerApplication({
   name: "@fabio-milheiro/navbar",
   app: () => System.import("@fabio-milheiro/navbar"),
-  activeWhen: isActive.navbar,
+  activeWhen: "/",
+});
+
+registerApplication({
+  name: "@fabio-milheiro/products",
+  app: () => System.import("@fabio-milheiro/products"),
+  activeWhen: "/products",
 });
 
 start({
